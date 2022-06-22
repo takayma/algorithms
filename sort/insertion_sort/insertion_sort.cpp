9d427bf24d1f1994cb2b30290fea5d78611b3cd2
takayma <imtakabaka@gmail.com>
2022-06-22 09:05:26 +0400

    commit

---
+++ sort/insertion_sort/insertion_sort.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> insertion_sort (vector<int> arr) {
+    for (int i = 1; i < arr.size(); ++i) {
+        int j = i;
+        while (arr[j - 1] >= arr[i] and j > 0) --j;
+        arr.insert(arr.begin() + j, arr[i]);
+        arr.erase(arr.begin() + i + 1);
+    }
+    return arr;
+}
+
+int main() {
+    vector<int> arr {1, -1, 3, 9, 0, 0, 4, 2, -5};
+    arr = insertion_sort(arr);
+    for (auto x : arr) cout << x << ' ';
+}
