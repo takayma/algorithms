360457ba389751a9fac8ba7a0ad1538e594a6461
takayma <imtakabaka@gmail.com>
2022-06-28 22:13:04 +0300

    commit

--- sort/insertion_sort/insertion_sort.cpp
+++ sort/insertion_sort/insertion_sort.cpp
@@ -3,14 +3,19 @@
 
 using namespace std;
 
-vector<int> insertion_sort (vector<int> arr) {
-    for (int i = 1; i < arr.size(); ++i) {
-        int j = i;
-        while (arr[j - 1] >= arr[i] and j > 0) --j;
-        arr.insert(arr.begin() + j, arr[i]);
-        arr.erase(arr.begin() + i + 1);
+vector<int> insertion_sort(vector<int> arr)
+{
+    for (int i = 1; i < arr.size(); ++i)
+    {
+        int x = arr[i];
+        int j = i - 1;
+        while (j >= 0 && arr[j] > x)
+        {
+            arr[j + 1] = arr[j];
+            --j;
+        }
+        arr[j + 1] = x;
     }
-    
     return arr;
 }
 
--- sort/insertion_sort/insertion_sort.py
+++ sort/insertion_sort/insertion_sort.py
@@ -1,9 +1,10 @@
 def insertion_sort(a):
     arr = a.copy()
     for i in range(1, len(arr)):
-        j = i
-        while arr[j - 1] >= arr[i] and j > 0:
+        x = arr[i]
+        j = i - 1
+        while (j >= 0 and arr[j] > x):
+            arr[j + 1] = arr[j]
             j -= 1
-        arr.insert(j, arr.pop(i))
-
+        arr[j + 1] = x
     return arr
